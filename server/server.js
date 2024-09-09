460e339441c62059263f42fd6bdd256f3a6b61c6
JaiShankar <jaishankar.a1603@gmail.com>
2024-09-09 22:55:53 +0530

    collabrative coding platform

---
+++ server/server.js
@@ -0,0 +1,60 @@
+const express=require('express');
+const {Server}=require('socket.io');
+const http=require('http');
+const ACTIONS = require('./Action');
+const app=express();
+
+const server=http.createServer(app);
+const io= new Server(server);
+const userSocketMap={};
+function getAllConnectedClients(roomId){
+   return  Array.from (io.sockets.adapter.rooms.get(roomId)|| []).map((socketId)=>{
+    return {
+        socketId,
+        username:userSocketMap[socketId],
+    }
+   });
+}
+io.on('connection',(socket)=>{
+    console.log('socket connected',socket.id);
+    socket.on(ACTIONS.JOIN,({roomId,username})=>{
+        userSocketMap[socket.id]=username;
+        socket.join(roomId);
+
+        const clients = getAllConnectedClients(roomId);
+        console.log(clients)
+        clients.forEach(({socketId})=>{
+            io.to(socketId).emit(ACTIONS.JOINED,{
+                clients,
+                username,
+                socketId:socket.id, 
+            })
+        })
+    })
+
+    socket.on(ACTIONS.CODE_CHANGE,({roomId,code})=>{
+        const username=userSocketMap[socket.id];
+        socket.in(roomId).emit(ACTIONS.CODE_CHANGE,{code,username});
+    })
+
+    socket.on(ACTIONS.SYNC_CODE,({socketId,code})=>{
+        const username=userSocketMap[socket.id];
+        io.to(socketId).emit(ACTIONS.CODE_CHANGE,{code,username});
+    })
+
+    socket.on('disconnecting',()=>{
+        const rooms=[...socket.rooms];
+        rooms.forEach((roomId)=>{
+            socket.in(roomId).emit(ACTIONS.DISCONNECTED,{
+                socketId:socket.id,
+                username:userSocketMap[socket.id]
+            })
+        })
+        delete userSocketMap[socket.id]
+        socket.leave();
+    })
+})
+
+server.listen(8000,()=>{
+    console.log("server started");
+})
